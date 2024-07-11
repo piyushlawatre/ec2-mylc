d17fc12e10d3f4819338561dc31716bc0229e230
piyush-lawatre-coditas <piyush.lawatre@coditas.com>
2024-07-11 14:52:46 +0530

    quote server

---
+++ quotes/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+import axios from "axios";
+
+const app = express();
+
+app.get("/quotes", async (req, res, next) => {
+  const { data } = await axios.get("https://dummyjson.com/quotes");
+  res.json(data);
+});
+
+app.listen(3002, "127.0.0.1", (req, res, next) => {
+  console.log("Server Quote Started");
+});
